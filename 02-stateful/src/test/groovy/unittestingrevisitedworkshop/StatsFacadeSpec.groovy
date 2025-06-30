7c1b1294ca298c3347d6b85b8493928b7bf338dc
Angelika Postaremczak <angelika.postaremczak@allegro.com>
2025-06-30 11:55:43 +0200

    Fix formatting

--- 02-stateful/src/test/groovy/unittestingrevisitedworkshop/StatsFacadeSpec.groovy
+++ 02-stateful/src/test/groovy/unittestingrevisitedworkshop/StatsFacadeSpec.groovy
@@ -6,42 +6,42 @@ import spock.lang.Specification
 class StatsFacadeSpec extends Specification {
     def "should return stats when account is found in repository"() {
         given:
-        def accountId = UUID.randomUUID()
-        // Teach the mock to always return specific stats
-        StatsRepository statsRepository = Mock(StatsRepository)
-        statsRepository.findByAccountId(accountId) >> Optional.of(new Stats(100, 1234, accountId, 300))
+            def accountId = UUID.randomUUID()
+            // Teach the mock to always return specific stats
+            StatsRepository statsRepository = Mock(StatsRepository)
+            statsRepository.findByAccountId(accountId) >> Optional.of(new Stats(100, 1234, accountId, 300))
 
-        StatsDownloader statsDownloader = Mock(StatsDownloader)
-        StatsFacade statsFacade = new StatsFacade(statsDownloader, statsRepository)
-        Account account = new Account(accountId, false)
+            StatsDownloader statsDownloader = Mock(StatsDownloader)
+            StatsFacade statsFacade = new StatsFacade(statsDownloader, statsRepository)
+            Account account = new Account(accountId, false)
 
         when:
-        Stats stats = statsFacade.getStatsFor(account)
+            Stats stats = statsFacade.getStatsFor(account)
 
         then:
-        0 * statsDownloader.downloadStatsFor(any())
+            0 * statsDownloader.downloadStatsFor(any())
     }
 
     def "should download stats from external systems and update repository when account not found in repository"() {
         given:
-        def accountId = UUID.randomUUID()
-        StatsRepository statsRepository = Mock(StatsRepository)
-        statsRepository.findByAccountId(accountId) >> Optional.empty()
+            def accountId = UUID.randomUUID()
+            StatsRepository statsRepository = Mock(StatsRepository)
+            statsRepository.findByAccountId(_ as UUID) >> Optional.empty()
 
-        def externalStats = new ExternalStats(accountId, 0, 0)
-        StatsDownloader statsDownloader = Mock(StatsDownloader)
-        statsDownloader.downloadStatsFor(accountId) >> externalStats
+            def externalStats = new ExternalStats(accountId, 0, 0)
+            StatsDownloader statsDownloader = Mock(StatsDownloader)
+            statsDownloader.downloadStatsFor(_ as UUID) >> externalStats
 
-        StatsFacade statsFacade = new StatsFacade(statsDownloader, statsRepository)
-        Account account = new Account(accountId, false)
+            StatsFacade statsFacade = new StatsFacade(statsDownloader, statsRepository)
+            Account account = new Account(accountId, false)
 
         when:
-        Stats stats = statsFacade.getStatsFor(account)
+            Stats stats = statsFacade.getStatsFor(account)
 
         then:
-        // Tried downloading external stats
-        1 * statsDownloader.downloadStatsFor(accountId)
-        // Repository was updated
-        1 * statsRepository.save(any())
+            // Tried downloading external stats
+            1 * statsDownloader.downloadStatsFor(accountId)
+            // Repository was updated
+            1 * statsRepository.save(any())
     }
 }
