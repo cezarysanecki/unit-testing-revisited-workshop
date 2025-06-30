7cbf7c160636ee4a87c7e0deff696c2d0e17bea3
Piotr Wygocki <piotr.wygocki@allegro.com>
2025-06-30 11:47:51 +0200

    add constructor

--- 02-stateful/src/main/java/pl/cezarysanecki/unittestingrevisitedworkshop/Stats.java
+++ 02-stateful/src/main/java/pl/cezarysanecki/unittestingrevisitedworkshop/Stats.java
@@ -16,4 +16,13 @@ public class Stats {
     public Long views;
     public Long likes;
 
+    public Stats() {
+    }
+
+    public Stats(Long id, Long likes, UUID accountId, Long views) {
+        this.id = id;
+        this.likes = likes;
+        this.accountId = accountId;
+        this.views = views;
+    }
 }
