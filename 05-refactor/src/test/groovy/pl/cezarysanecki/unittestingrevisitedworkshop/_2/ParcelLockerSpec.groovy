ead1ab42440493ba9398692cc34022c71c853825
Angelika Postaremczak <angelika.postaremczak@allegro.com>
2025-06-30 15:05:58 +0200

    Start ParcelLockerSpec

---
+++ 05-refactor/src/test/groovy/pl/cezarysanecki/unittestingrevisitedworkshop/_2/ParcelLockerSpec.groovy
@@ -0,0 +1,26 @@
+package pl.cezarysanecki.unittestingrevisitedworkshop._2
+
+import spock.lang.Specification
+
+class ParcelLockerSpec extends Specification {
+    def "Empty"() {
+    }
+
+    def "LockFor"() {
+    }
+
+    def "Prolong"() {
+    }
+
+    def "Open"() {
+    }
+
+    def "Release"() {
+    }
+
+    def "GetAssignedTo"() {
+    }
+
+    def "GetLockUntil"() {
+    }
+}
