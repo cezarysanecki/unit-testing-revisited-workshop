416e07a43a62ae81d503fb4932cd6a74e7856acf
Angelika Postaremczak <angelika.postaremczak@allegro.com>
2025-06-30 12:27:45 +0200

    Fix failing tests

--- 02-stateful/src/test/groovy/unittestingrevisitedworkshop/StatsFacadeSpec.groovy
+++ 02-stateful/src/test/groovy/unittestingrevisitedworkshop/StatsFacadeSpec.groovy
@@ -6,42 +6,54 @@ import spock.lang.Specification
 class StatsFacadeSpec extends Specification {
     def "should return stats when account is found in repository"() {
         given:
-            def accountId = UUID.randomUUID()
-            // Teach the mock to always return specific stats
-            StatsRepository statsRepository = Mock(StatsRepository)
-            statsRepository.findByAccountId(accountId) >> Optional.of(new Stats(100, 1234, accountId, 300))
+        def accountId = UUID.randomUUID()
+        def views = 100
+        def statsId = 1234
+        def likes = 300
 
-            StatsDownloader statsDownloader = Mock(StatsDownloader)
-            StatsFacade statsFacade = new StatsFacade(statsDownloader, statsRepository)
-            Account account = new Account(accountId, false)
+        // Teach the mock to always return specific stats
+        StatsRepository statsRepository = Mock(StatsRepository)
+        statsRepository.findByAccountId(_ as UUID) >> Optional.of(new Stats(views, statsId, accountId, likes))
+
+        StatsDownloader statsDownloader = Mock(StatsDownloader)
+        StatsFacade statsFacade = new StatsFacade(statsDownloader, statsRepository)
+        Account account = new Account(accountId, false)
 
         when:
-            Stats stats = statsFacade.getStatsFor(account)
+        Stats stats = statsFacade.getStatsFor(account)
 
         then:
-            0 * statsDownloader.downloadStatsFor(any())
+        0 * statsDownloader.downloadStatsFor(any())
+        // Verify stats
+        stats.accountId == accountId
+        stats.id == statsId
+        stats.views == views
+        stats.likes == likes
     }
 
     def "should download stats from external systems and update repository when account not found in repository"() {
         given:
-            def accountId = UUID.randomUUID()
-            StatsRepository statsRepository = Mock(StatsRepository)
-            statsRepository.findByAccountId(_ as UUID) >> Optional.empty()
+        def accountId = UUID.randomUUID()
+        def views = 100
+        def statsId = 1234
+        def likes = 300
 
-            def externalStats = new ExternalStats(accountId, 0, 0)
-            StatsDownloader statsDownloader = Mock(StatsDownloader)
-            statsDownloader.downloadStatsFor(_ as UUID) >> externalStats
+        StatsRepository statsRepository = Mock(StatsRepository)
+        statsRepository.findByAccountId(_ as UUID) >> Optional.empty()
 
-            StatsFacade statsFacade = new StatsFacade(statsDownloader, statsRepository)
-            Account account = new Account(accountId, false)
+        def externalStats = new ExternalStats(accountId, views, likes)
+        StatsDownloader statsDownloader = Mock(StatsDownloader)
+
+        StatsFacade statsFacade = new StatsFacade(statsDownloader, statsRepository)
+        Account account = new Account(accountId, false)
 
         when:
-            Stats stats = statsFacade.getStatsFor(account)
+        Stats stats = statsFacade.getStatsFor(account)
 
         then:
-            // Tried downloading external stats
-            1 * statsDownloader.downloadStatsFor(accountId)
-            // Repository was updated
-            1 * statsRepository.save(any())
+        // Tried downloading external stats
+        1 * statsDownloader.downloadStatsFor(accountId) >> externalStats
+        // Repository was updated
+        1 * statsRepository.save(_ as Stats)
     }
 }
