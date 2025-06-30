3be9819b73250e7d1deae4974c040c9aff9a4b19
Angelika Postaremczak <angelika.postaremczak@allegro.com>
2025-06-30 10:22:50 +0200

    Add DriverLicenseSpec

---
+++ 01-basics/src/test/groovy/pl/cezarysanecki/unittestingrevisitedworkshop/DriverLicenseSpec.groovy
@@ -0,0 +1,37 @@
+package pl.cezarysanecki.unittestingrevisitedworkshop
+
+import spock.lang.Specification
+
+class DriverLicenseSpec extends Specification {
+    def "should create driver license with valid license number"() {
+        when:
+        def license = DriverLicense.withLicense("ABCDE12")
+
+        then:
+        license.print() == "ABCDE12"
+    }
+
+    def "should fail when license does not match required pattern"() {
+        when:
+        DriverLicense.withLicense("LICENSE_NOT_MATCHING_PATTERN")
+
+        then:
+        thrown(IllegalArgumentException)
+    }
+
+    def "should fail when license is empty"() {
+        when:
+        DriverLicense.withLicense("")
+
+        then:
+        thrown(IllegalArgumentException)
+    }
+
+    def "should fail when license is null"() {
+        when:
+        DriverLicense.withLicense(null)
+
+        then:
+        thrown(IllegalArgumentException)
+    }
+}
