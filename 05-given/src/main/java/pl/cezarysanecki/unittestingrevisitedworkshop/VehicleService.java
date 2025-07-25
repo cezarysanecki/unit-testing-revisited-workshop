54a491551fbcf9f3ffcc7d35d86710999a4dd2cf
Cezary Sanecki <saneckicezary@gmail.com>
2025-07-25 15:18:34 +0200

    Prepared different cases

--- 05-given/src/main/java/pl/cezarysanecki/unittestingrevisitedworkshop/VehicleService.java
+++ 05-given/src/main/java/pl/cezarysanecki/unittestingrevisitedworkshop/VehicleService.java
@@ -1,10 +1,15 @@
 package pl.cezarysanecki.unittestingrevisitedworkshop;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class VehicleService {
+
+    private static final List<String> INCREASED_RISK_MAKES = List.of(
+            "AUDI", "BMW"
+    );
 
     private final Map<Parts, Double> partsPrices;
     private final DateProvider dateProvider;
@@ -33,61 +38,39 @@ public class VehicleService {
 
         boolean isOldCar = (currentYear - year) > 10;
         boolean isYoungCar = (currentYear - year) < 3;
-        boolean highMileage = mileage > 200_000;
+        boolean highMileage = mileage >= 200_000;
         boolean inspectionExpired = lastInspection.isBefore(currentDate.minusYears(2));
-        boolean engineDamaged = damaged.contains(Parts.ENGINE);
-        boolean gearboxDamaged = damaged.contains(Parts.GEARBOX);
         boolean allPartsDamaged = damaged.containsAll(Set.of(Parts.values()));
+        boolean isIncreasedRiskMake = INCREASED_RISK_MAKES.contains(vehicle.make());
 
-        double total;
-        double engineCost = 0;
-        double gearboxCost = 0;
-        double suspensionCost = 0;
-        double paintCost = 0;
+        double total = 0;
+        for (Parts parts : damaged) {
+            total += partsPrices.getOrDefault(parts, 0.0);
+        }
 
-        for (Parts part : damaged) {
-            double partCost = partsPrices.getOrDefault(part, 0.0);
+        double multiplier = 1.0;
 
-            if (part == Parts.PAINT && isOldCar) {
-                paintCost = partCost;
-            } else if (part == Parts.PAINT) {
-                paintCost = 0;
-            } else if (part == Parts.SUSPENSION && isYoungCar) {
-                suspensionCost += 0;
-            } else if (part == Parts.SUSPENSION) {
-                suspensionCost = partCost;
-            } else if (part == Parts.ENGINE) {
-                double percentage = 1.0;
+        if (isOldCar) {
+            multiplier += 0.05;
+        } else if (isYoungCar) {
+            multiplier -= 0.05;
+        }
 
-                if (highMileage) {
-                    percentage += 0.2;
-                }
-                if (isOldCar) {
-                    percentage += 0.3;
-                }
-
-                engineCost = (int) Math.round(partCost * percentage);
-            } else if (part == Parts.GEARBOX) {
-                gearboxCost = partCost;
+        if (highMileage) {
+            if (isIncreasedRiskMake) {
+                multiplier += 0.10;
+            } else {
+                multiplier += 0.05;
             }
         }
 
-        total = engineCost + gearboxCost + suspensionCost + paintCost;
-
-        if (engineDamaged && gearboxDamaged) {
-            double sum = engineCost + gearboxCost;
-            double discount = Math.round(sum * 0.15);
-            total -= discount;
-        }
-
-        double percentage = 1.0;
         if (inspectionExpired) {
-            percentage += 0.1;
+            multiplier += 0.05;
         }
-        if (allPartsDamaged && isOldCar) {
-            percentage -= -0.2;
+        if (allPartsDamaged) {
+            multiplier -= 0.10;
         }
 
-        return Math.round(total * percentage * 100.0) / 100.0;
+        return Math.round(total * multiplier * 100.0) / 100.0;
     }
 }
--- 05-given/src/test/groovy/pl/cezarysanecki/unittestingrevisitedworkshop/VehicleServiceSpec.groovy
+++ 05-given/src/test/groovy/pl/cezarysanecki/unittestingrevisitedworkshop/VehicleServiceSpec.groovy
@@ -4,21 +4,30 @@ import spock.lang.Specification
 
 import java.time.LocalDate
 
+import static pl.cezarysanecki.unittestingrevisitedworkshop.Parts.*
+
 class VehicleServiceSpec extends Specification {
+
+    Map<Parts, Double> EXAMPLE_PARTS_PRICES = Map.of(
+            ENGINE, 5_000.0d,
+            GEARBOX, 4_000.0d,
+            SUSPENSION, 10_000.0d,
+            PAINT, 1_000.0d,
+    )
 
     LocalDate CURRENT_DATE = LocalDate.now()
 
     FakeDateProvider dateProvider = new FakeDateProvider(CURRENT_DATE)
 
-    def "count full price for paint when vehicle is too old"() {
+    def "take into account discount for all damaged parts"() {
 
     }
 
-    def "take into account that engine repair cost is more expensive for too old vehicle"() {
+    def "take into account rise for expired inspection"() {
 
     }
 
-    def "provide discount for too old vehicle with all damaged parts"() {
+    def "take into account rise for high milage BMW vehicle"() {
 
     }
 
