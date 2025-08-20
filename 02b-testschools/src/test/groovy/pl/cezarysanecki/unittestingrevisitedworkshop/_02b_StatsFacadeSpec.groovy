2297bc1de71de6ba30d5cf6daa1eab5831e0b4b2
Cezary Sanecki <saneckicezary@gmail.com>
2025-08-20 15:50:44 +0200

    Added solutin test for 2a and 2b

--- 02b-testschools/src/test/groovy/pl/cezarysanecki/unittestingrevisitedworkshop/_02b_StatsFacadeSpec.groovy
+++ 02b-testschools/src/test/groovy/pl/cezarysanecki/unittestingrevisitedworkshop/_02b_StatsFacadeSpec.groovy
@@ -20,11 +20,43 @@ class _02b_StatsFacadeSpec extends Specification {
     def sut = new StatsFacade(statsDownload, statsRepository)
 
     def "update stats for existing account using downloaded stats"() {
+        given:
+        importantStatsSystem.downloadStatsFor(anAccount.id()) >> new ExternalStats(anAccount.id(), 100, 20)
+        and:
+        additionalStatsSystem.downloadStatsFor(anAccount.id()) >> new ExternalStats(anAccount.id(), 100, 20)
+        and:
+        statsRepository.findByAccountId(anAccount.id()) >> Optional.of(new Stats(anAccount.id(), 10, 2))
+        and:
+        statsRepository.save(_ as Stats) >> { Stats stats ->
+            return stats
+        }
 
+        when:
+        def result = sut.getStatsFor(anAccount)
+
+        then:
+        result.views == 100
+        result.likes == 20
     }
 
     def "create account with downloaded stats when it does not exist"() {
+        given:
+        importantStatsSystem.downloadStatsFor(anAccount.id()) >> new ExternalStats(anAccount.id(), 100, 20)
+        and:
+        additionalStatsSystem.downloadStatsFor(anAccount.id()) >> new ExternalStats(anAccount.id(), 100, 20)
+        and:
+        statsRepository.findByAccountId(anAccount.id()) >> Optional.empty()
+        and:
+        statsRepository.save(_ as Stats) >> { Stats stats ->
+            return stats
+        }
 
+        when:
+        def result = sut.getStatsFor(anAccount)
+
+        then:
+        result.views == 100
+        result.likes == 20
     }
 
 }
