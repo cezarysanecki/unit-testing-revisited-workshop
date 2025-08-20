f9c99998f6d914ce89c9fb08de6bb438addd0b89
Cezary Sanecki <saneckicezary@gmail.com>
2025-08-20 18:03:41 +0200

    Added solutin test for 6 module

--- 06-assert/src/test/groovy/pl/cezarysanecki/unittestingrevisitedworkshop/VehicleRepairServiceSpec.groovy
+++ 06-assert/src/test/groovy/pl/cezarysanecki/unittestingrevisitedworkshop/VehicleRepairServiceSpec.groovy
@@ -4,10 +4,13 @@ import spock.lang.Specification
 
 import java.time.LocalDate
 
+import static java.util.EnumSet.allOf
 import static pl.cezarysanecki.unittestingrevisitedworkshop.Parts.ENGINE
 import static pl.cezarysanecki.unittestingrevisitedworkshop.Parts.GEARBOX
 import static pl.cezarysanecki.unittestingrevisitedworkshop.Parts.PAINT
 import static pl.cezarysanecki.unittestingrevisitedworkshop.Parts.SUSPENSION
+import static pl.cezarysanecki.unittestingrevisitedworkshop.VehicleTestBuilder.aVehicle
+import static pl.cezarysanecki.unittestingrevisitedworkshop.VehicleTestBuilder.typicalAgedVehicle
 
 class VehicleRepairServiceSpec extends Specification {
 
@@ -24,15 +27,56 @@ class VehicleRepairServiceSpec extends Specification {
     def sut = new VehicleRepairService(dateProvider, EXAMPLE_PARTS_PRICES)
 
     def "reject repairing damaged parts of increased risk vehicle make"() {
+        given:
+        def vehicle = typicalAgedVehicle(CURRENT_DATE)
+                .withMake("BMW")
+                .withDamagedParts([GEARBOX])
+                .build()
 
+        when:
+        def result = sut.prepareRepairEvaluation(vehicle)
+
+        then:
+        ServiceResultAssertObject.of(result)
+                .theSameVehicle(vehicle.id())
+                .rejected()
+                .noCharge()
+                .noHandledParts()
     }
 
     def "accept all parts except engine for old vehicle"() {
+        given:
+        def vehicle = aVehicle()
+                .withDamagedParts(allOf(Parts.class))
+                .old(CURRENT_DATE)
+                .build()
 
+        when:
+        def result = sut.prepareRepairEvaluation(vehicle)
+
+        then:
+        ServiceResultAssertObject.of(result)
+                .theSameVehicle(vehicle.id())
+                .accepted()
+                .charge(15_000.0d)
+                .allPartsBut(ENGINE)
     }
 
     def "include discount when all parts handled by service are accepted"() {
+        given:
+        def vehicle = typicalAgedVehicle(CURRENT_DATE)
+                .withDamagedParts(allOf(Parts.class))
+                .build()
 
+        when:
+        def result = sut.prepareRepairEvaluation(vehicle)
+
+        then:
+        ServiceResultAssertObject.of(result)
+                .theSameVehicle(vehicle.id())
+                .accepted()
+                .charge(18_000.0d)
+                .allParts()
     }
 
 }
