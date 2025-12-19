6d9104ca06e4daf1b079828f0c2f54d11447d984
Cezary Sanecki <saneckicezary@gmail.com>
2025-12-19 17:18:02 +0100

    Added 3 solution

--- 03-refactor/src/main/java/pl/cezarysanecki/unittestingrevisitedworkshop/calculator/OrderPriceCalculator.java
+++ 03-refactor/src/main/java/pl/cezarysanecki/unittestingrevisitedworkshop/calculator/OrderPriceCalculator.java
@@ -3,15 +3,20 @@ package pl.cezarysanecki.unittestingrevisitedworkshop.calculator;
 public class OrderPriceCalculator {
 
     private final OrderRepository orderRepository;
+    private final ExternalPriceCalculatorInterface externalPriceCalculatorInterface;
 
-    public OrderPriceCalculator(OrderRepository orderRepository) {
+    public OrderPriceCalculator(
+            OrderRepository orderRepository,
+            ExternalPriceCalculatorInterface externalPriceCalculatorInterface
+    ) {
         this.orderRepository = orderRepository;
+        this.externalPriceCalculatorInterface = externalPriceCalculatorInterface;
     }
 
     public double computeFinalPriceFor(Long orderId) {
         Order order = orderRepository.findBy(orderId);
 
-        return ExternalPriceCalculator.computeFinalPrice(order.totalPrice());
+        return externalPriceCalculatorInterface.computeFinalPrice(order.totalPrice());
     }
 
 }
--- 03-refactor/src/test/groovy/pl/cezarysanecki/unittestingrevisitedworkshop/calculator/OrderPriceCalculatorSpec.groovy
+++ 03-refactor/src/test/groovy/pl/cezarysanecki/unittestingrevisitedworkshop/calculator/OrderPriceCalculatorSpec.groovy
@@ -5,11 +5,28 @@ import spock.lang.Specification
 class OrderPriceCalculatorSpec extends Specification {
 
     def orderRepository = new InMemoryOrderRepository()
+    def externalPriceCalculator = new ExternalPriceCalculatorInterface() {
+        @Override
+        double computeFinalPrice(double price) {
+            return price * 1.2
+        }
+    }
 
-    def sut = new OrderPriceCalculator(orderRepository)
+    def sut = new OrderPriceCalculator(orderRepository, externalPriceCalculator)
 
     def "calculate price of order taking into account tax"() {
+        given:
+        def order = new Order(12L, [
+                new Order.Product(133L, 10, 2.0),
+        ])
+        and:
+        orderRepository.save(order)
 
+        when:
+        def result = sut.computeFinalPriceFor(order.orderId())
+
+        then:
+        result == 24.0D
     }
 
 }
