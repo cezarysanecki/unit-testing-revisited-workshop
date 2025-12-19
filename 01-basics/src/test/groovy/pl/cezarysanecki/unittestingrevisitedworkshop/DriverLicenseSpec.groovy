a53711e30ec9601af1f1dc2c4888cc9ced5baafa
Cezary Sanecki <saneckicezary@gmail.com>
2025-12-19 17:11:23 +0100

    Added 1 solution

--- 01-basics/src/test/groovy/pl/cezarysanecki/unittestingrevisitedworkshop/DriverLicenseSpec.groovy
+++ 01-basics/src/test/groovy/pl/cezarysanecki/unittestingrevisitedworkshop/DriverLicenseSpec.groovy
@@ -4,6 +4,34 @@ import spock.lang.Specification
 
 class DriverLicenseSpec extends Specification {
 
-    // Write tests here...
+    def "allow to create driver licence without validation"() {
+        when:
+        def driverLicence = DriverLicense.withoutValidation("123")
+
+        then:
+        driverLicence.print() == "123"
+    }
+
+    def "fail to create driver licence when value #"() {
+        when:
+        DriverLicense.withLicense(value)
+
+        then:
+        thrown(IllegalArgumentException)
+
+        where:
+        value  | testCase
+        ""     | "is empty"
+        null   | "is null"
+        "fake" | "does not match pattern"
+    }
+
+    def "allow to create driver licence matching pattern"() {
+        when:
+        def driverLicence = DriverLicense.withLicense("ABCDE12")
+
+        then:
+        driverLicence.print() == "ABCDE12"
+    }
 
 }
