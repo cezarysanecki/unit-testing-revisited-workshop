49acb27e93a77d594252e3ba1e79ba67a597a452
Cezary Sanecki <saneckicezary@gmail.com>
2025-12-19 17:16:51 +0100

    Added 2e solution

--- 02e-spring/src/test/groovy/pl/cezarysanecki/unitestingrevisitedworkshop/_02e_StatsFacadeSpec.groovy
+++ 02e-spring/src/test/groovy/pl/cezarysanecki/unitestingrevisitedworkshop/_02e_StatsFacadeSpec.groovy
@@ -1,7 +1,10 @@
 package pl.cezarysanecki.unitestingrevisitedworkshop
 
 import pl.cezarysanecki.unittestingrevisitedworkshop.Account
+import pl.cezarysanecki.unittestingrevisitedworkshop.ExternalStats
 import pl.cezarysanecki.unittestingrevisitedworkshop.InconsistentDataEvent
+import pl.cezarysanecki.unittestingrevisitedworkshop.Stats
+import pl.cezarysanecki.unittestingrevisitedworkshop.StatsConfig
 import pl.cezarysanecki.unittestingrevisitedworkshop.StatsDownloader
 import pl.cezarysanecki.unittestingrevisitedworkshop.StatsFacade
 import pl.cezarysanecki.unittestingrevisitedworkshop.helpers.InMemoryAdditionalStatsSystem
@@ -22,20 +25,60 @@ class _02e_StatsFacadeSpec extends Specification {
     def additionalStatsSystem = new InMemoryAdditionalStatsSystem()
     def eventPublisher = new InMemoryEventPublisher<InconsistentDataEvent>()
 
-    def statsDownload = new StatsDownloader(
+    def sut = new StatsConfig().statsFacade(
+            statsRepository,
             importantStatsSystem,
             additionalStatsSystem,
             eventPublisher
     )
 
-    def sut = new StatsFacade(statsDownload, statsRepository)
-
     def "update stats for existing account using downloaded stats"() {
+        given:
+        importantStatsSystem.store(new ExternalStats(anAccount.id(), 100, 20))
+        and:
+        additionalStatsSystem.store(new ExternalStats(anAccount.id(), 100, 20))
+        and:
+        statsRepository.save(new Stats(anAccount.id(), 10, 2))
 
+        when:
+        def result = sut.getStatsFor(anAccount)
+
+        then:
+        result.views == 100
+        result.likes == 20
     }
 
     def "create account with downloaded stats when it does not exist"() {
+        given:
+        importantStatsSystem.store(new ExternalStats(anAccount.id(), 100, 20))
+        and:
+        additionalStatsSystem.store(new ExternalStats(anAccount.id(), 100, 20))
 
+        when:
+        def result = sut.getStatsFor(anAccount)
+
+        then:
+        result.views == 100
+        result.likes == 20
     }
 
+    def "allow to update stats adhoc"() {
+        given:
+        statsRepository.save(new Stats(anAccount.id(), 10, 2))
+
+        when:
+        def result = sut.updateStatsAdHocFor(anAccount, 200, 10)
+
+        then:
+        result.views == 200
+        result.likes == 10
+    }
+
+    def "do not allow to update stats adhoc when likes are greater than views"() {
+        when:
+        sut.updateStatsAdHocFor(anAccount, 200, 201)
+
+        then:
+        thrown(IllegalArgumentException)
+    }
 }
