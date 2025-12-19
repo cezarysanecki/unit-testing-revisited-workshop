a669f31c4a91a53085810add5384fcb9200565a5
Cezary Sanecki <saneckicezary@gmail.com>
2025-12-19 17:22:28 +0100

    Added 5 solution

--- 05-given/src/test/groovy/pl/cezarysanecki/unittestingrevisitedworkshop/VehicleServiceSpec.groovy
+++ 05-given/src/test/groovy/pl/cezarysanecki/unittestingrevisitedworkshop/VehicleServiceSpec.groovy
@@ -8,6 +8,7 @@ import static pl.cezarysanecki.unittestingrevisitedworkshop.Parts.ENGINE
 import static pl.cezarysanecki.unittestingrevisitedworkshop.Parts.GEARBOX
 import static pl.cezarysanecki.unittestingrevisitedworkshop.Parts.PAINT
 import static pl.cezarysanecki.unittestingrevisitedworkshop.Parts.SUSPENSION
+import static pl.cezarysanecki.unittestingrevisitedworkshop.VehicleTestBuilder.typicalAgedVehicle
 
 class VehicleServiceSpec extends Specification {
 
@@ -24,15 +25,45 @@ class VehicleServiceSpec extends Specification {
     def sut = new VehicleService(dateProvider, EXAMPLE_PARTS_PRICES)
 
     def "take into account discount for all damaged parts"() {
+        given:
+        def vehicle = typicalAgedVehicle(CURRENT_DATE)
+                .withDamagedParts(EnumSet.allOf(Parts.class))
+                .build()
 
+        when:
+        def result = sut.calculateTotalRepairCost(vehicle)
+
+        then:
+        result == 18_000.0d
     }
 
     def "take into account rise for expired inspection"() {
+        given:
+        def vehicle = typicalAgedVehicle(CURRENT_DATE)
+                .withDamagedParts([GEARBOX])
+                .expiredInspectionDate(CURRENT_DATE)
+                .build()
 
+        when:
+        def result = sut.calculateTotalRepairCost(vehicle)
+
+        then:
+        result == 4_200.0d
     }
 
     def "take into account rise for high mileage BMW vehicle"() {
+        given:
+        def vehicle = typicalAgedVehicle(CURRENT_DATE)
+                .withMake("BMW")
+                .withDamagedParts([GEARBOX])
+                .highMileage()
+                .build()
 
+        when:
+        def result = sut.calculateTotalRepairCost(vehicle)
+
+        then:
+        result == 4_400.0d
     }
 
 }
