9bbc29da3269a4eaec59ced3f4f3ebd57fbe1eef
Cezary Sanecki <saneckicezary@gmail.com>
2025-12-19 17:12:48 +0100

    Added 2a solution

--- 02a-stateful/src/test/groovy/pl/cezarysanecki/unittestingrevisitedworkshop/_02a_StatsFacadeSpec.groovy
+++ 02a-stateful/src/test/groovy/pl/cezarysanecki/unittestingrevisitedworkshop/_02a_StatsFacadeSpec.groovy
@@ -12,11 +12,39 @@ class _02a_StatsFacadeSpec extends Specification {
     def sut = new StatsFacade(statsDownload, statsRepository)
 
     def "update stats for existing account using downloaded stats"() {
+        given:
+        statsDownload.downloadStatsFor(anAccount.id()) >> new ExternalStats(anAccount.id(), 100, 20)
+        and:
+        statsRepository.findByAccountId(anAccount.id()) >> Optional.of(new Stats(anAccount.id(), 10, 2))
+        and:
+        statsRepository.save(_ as Stats) >> { Stats stats ->
+            return stats
+        }
 
+        when:
+        def result = sut.getStatsFor(anAccount)
+
+        then:
+        result.views == 100
+        result.likes == 20
     }
 
     def "create account with downloaded stats when it does not exist"() {
+        given:
+        statsDownload.downloadStatsFor(anAccount.id()) >> new ExternalStats(anAccount.id(), 100, 20)
+        and:
+        statsRepository.findByAccountId(anAccount.id()) >> Optional.empty()
+        and:
+        statsRepository.save(_ as Stats) >> { Stats stats ->
+            return stats
+        }
 
+        when:
+        def result = sut.getStatsFor(anAccount)
+
+        then:
+        result.views == 100
+        result.likes == 20
     }
 
 }
